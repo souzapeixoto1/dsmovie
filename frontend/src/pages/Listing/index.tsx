9fb322f2333ed6848b5d971fe201c61d489ed487
Leonardo Peixoto <peixoto.souza1999@gmail.com>
2022-01-12 02:38:54 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,40 @@
 import Pagination from "components/Pagination";
+import MovieStars from "components/MovieStars";
+import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 
-function Listing(){
-    return(
-        <Pagination />
-    )
+function Listing() {
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-1x-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-1x-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-1x-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-1x-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-1x-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-1x-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+            </div>
+
+
+        </>
+    );
 }
 
 export default Listing;
